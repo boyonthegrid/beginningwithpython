d06e04f26ea3bfe1adb6bfc65ea497e307bb86a1
boy from the grid <272283433+boyonthegrid@users.noreply.github.com>
2026-04-14 20:05:33 +0200

    Add files via upload
    the simplest form of input

---
+++ input.py
@@ -0,0 +1,9 @@
+#a simple script to sexplain how to get an input from the console
+
+
+def main():
+    #this input() will save the text you type and then print it instantly 
+    we=input("Type your input here:")
+    print(we)
+
+main()
